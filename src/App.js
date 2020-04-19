86f81330ba7aa532ff1105f287714a5d9f2e3663
George Gardner <gpgardner@yahoo.com>
2020-04-19 12:03:24 -0400

    react to-do list tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import Note from './Note/Note.jsx'
+import NoteForm from './NoteForm/NoteForm.jsx'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor (props){
+    super(props);
+    this.addNote = this.addNote.bind(this);
+
+    //Going to setup the React state of our component
+    this.state = {
+      notes: [
+        {id: 1, noteContent: "Note 1 here!"},
+        {id: 2, noteContent: "Note 2 here!"},
+      ],
+    }
+  }
+
+  addNote(note){
+    //push the note onto the notes array.
+    const previousNotes = this.state.notes;
+    previousNotes.push({ id: previousNotes.length + 1, noteContent: note});
+    this.setState({
+        notes: previousNotes,
+    })
+  }
+
+  render(){
+    return (
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <div className="heading">React & FireBase To-Do List</div>
+        </div>
+        <div className="notesBody">
+          {
+            this.state.notes.map((note) => {
+              return(
+                <Note noteContent={note.noteContent} noteId={note.id} key={note.id}/>
+                )
+              })
+          }
+        </div>
+        <div className="notesFooter">
+          <NoteForm addNote={this.addNote}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
